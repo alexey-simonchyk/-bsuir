c16f5d9413600b6411fb50c6c0e96ac63f086135
Alexey <alexey.simonchyk@gmail.com>
2017-10-12 21:32:26 +0300

    Added generating polyharmonic and harmonic signals with restoring

--- COS/lab3/main.py
+++ COS/lab3/main.py
@@ -7,14 +7,35 @@ import matplotlib.pyplot as plt
 AMPLITUDES = [3, 5, 6, 8, 10, 13, 16]
 BEGIN_PHASES = [pi/6, pi/4, pi/3, pi/2, pi/4, pi]
 
+NUMBER_HARMONICS = 30
+
 
 def main():
-    signal = util.harmonic_signal(2048, 2048)
-    sum_sin, sum_cos, amplitude, harmonic_begin_phase = util.fourier(signal)
-    signal_2 = util.restore_signal(2048, 2048, harmonic_begin_phase, amplitude)
-    x = np.arange(2048)
-    plt.plot(x, signal, 'r-', x, signal_2, 'g-')
+
+    # uncomment for harmonic/polyharmonic signal
+    signal, restored_signal, restored_signal_without_begin_phase = harmonic()
+    # signal, restored_signal, restored_signal_without_begin_phase = polyharmonic()
+    x = np.arange(len(signal))
+    plt.plot(x, signal, 'r-', x, restored_signal, 'g--', x, restored_signal_without_begin_phase, 'b--')
     plt.show()
+
+
+def harmonic():
+    signal = util.harmonic_signal()
+    amplitude, harmonic_begin_phase = util.fourier(signal)
+    restored_signal = util.restore_signal(harmonic_begin_phase, amplitude)
+    restored_signal_without_begin_phase = util.restore_signal(0, amplitude)
+    return signal, restored_signal, restored_signal_without_begin_phase
+
+
+def polyharmonic():
+    signal = util.polyharmonic_signal(NUMBER_HARMONICS, AMPLITUDES, BEGIN_PHASES)
+    results = list(util.fourier(signal, j) for j in range(1, int(len(signal) / 2)))
+    amplitudes = list(temp[0] for temp in results)
+    begin_phases = list(temp[1] for temp in results)
+    restored_signal = util.restore_polyharmonic_signal(amplitudes, begin_phases)
+    restored_signal_without_begin_phases = util.restore_polyharmonic_signal(amplitudes, np.zeros(len(results)))
+    return signal, restored_signal, restored_signal_without_begin_phases
 
 
 main()
--- COS/lab3/util.py
+++ COS/lab3/util.py
@@ -1,13 +1,27 @@
 import numpy as np
 from math import pi, cos, sin, sqrt, atan2
+from random import choices
 
 
-def harmonic_signal(sampling, buffer_size, begin_phase=3 * pi / 4, amplitude=30):
+def harmonic_signal(sampling=2048, buffer_size=2048, begin_phase=3 * pi / 4, amplitude=30, frequency=1):
     result = []
     for i in range(buffer_size):
-        value = amplitude * cos(2 * pi * i / sampling - begin_phase)
+        value = amplitude * cos(2 * pi * i * frequency / sampling - begin_phase)
         result.append(value)
     return np.array(result)
+
+
+def polyharmonic_signal(number_signals, amplitudes_input, begin_phases_input, sampling=2048, buffer_size=2048):
+    signal = []
+    amplitudes = choices(amplitudes_input, k=number_signals)
+    begin_phases = choices(begin_phases_input, k=number_signals)
+
+    for i in range(buffer_size):
+        value = 0
+        for frequency, (amplitude, begin_phase) in enumerate(zip(amplitudes, begin_phases)):
+            value += amplitude * cos(2 * pi * i * frequency / sampling - begin_phase)
+        signal.append(value)
+    return np.array(signal)
 
 
 def fourier(signal, j=1):
@@ -22,13 +36,22 @@ def fourier(signal, j=1):
     sum_sin *= 2 / length
     sum_cos *= 2 / length
     harmonic_begin_phase = atan2(sum_sin, sum_cos)
-    return sum_sin, sum_cos, sqrt(sum_sin ** 2 + sum_cos ** 2), harmonic_begin_phase
+    return sqrt(sum_sin ** 2 + sum_cos ** 2), harmonic_begin_phase
 
 
-def restore_signal(sampling, buffer_size, harmonic_begin_phase, harmonic_amplitude):
+def restore_signal(harmonic_begin_phase, harmonic_amplitude, sampling=2048, buffer_size=2048):
     result = []
     for i in range(buffer_size):
         value = harmonic_amplitude * cos(2 * pi * i / sampling - harmonic_begin_phase)
         result.append(value)
     return np.array(result)
 
+
+def restore_polyharmonic_signal(amplitudes, begin_phases, buffer_size=2048, sampling=2048):
+    restored_signal = []
+    for i in range(buffer_size):
+        value = amplitudes[0] / 2
+        for j in range(len(amplitudes)):
+            value += amplitudes[j] * cos(2 * pi * (j + 1) * i / sampling - begin_phases[j])
+        restored_signal.append(value)
+    return np.array(restored_signal)
