39b6ccab18bd746c8280b3273174a9e7d737c848
Alexey <alexey.simonchyk@gmail.com>
2017-10-23 22:20:10 +0300

    Fixed 4lab saammod

--- SAaMMod/4lab/elements.py
+++ SAaMMod/4lab/elements.py
@@ -1,21 +1,22 @@
 import random
+from collections import deque
 
 
 class BaseElement:
     def __init__(self, next_element):
-        self._status = 0
         self._next_element = next_element
 
     def handle(self):
-        print()
+        return None
 
-    @property
+    def process(self, bid):
+        return None
+
+    def can_accept_bid(self):
+        return None
+
     def status(self):
-        return self._status
-
-    @status.setter
-    def status(self, status):
-        self._status = status
+        return 0
 
     @property
     def next_element(self):
@@ -32,6 +33,7 @@ class Generator(BaseElement):
         super().__init__(next_element)
         self.__generate_probability = generate_probability
         self.__generated_number = 0
+        self.__rejected_counter = Counter()
 
     @property
     def generated_number(self):
@@ -41,10 +43,9 @@ class Generator(BaseElement):
         temp = random.randint(0, 100)
         if temp < self.__generate_probability:
             self.__generated_number += 1
-            if type(self.next_element) is Queue:
-                self.next_element.put_in_queue()
-            elif self.next_element.status == 0:
-                self.next_element.status += 1
+            bid = self.next_element.process(Bid())
+            if bid:
+                self.__rejected_counter.process(bid)
 
 
 class Handler(BaseElement):
@@ -52,15 +53,27 @@ class Handler(BaseElement):
     def __init__(self, probability, next_element):
         super().__init__(next_element)
         self.__probability = probability
+        self.__current_bid = None
+
+    def status(self):
+        return 1 if self.__current_bid else 0
+
+    def process(self, bid):
+        if self.__current_bid:
+            return bid
+        else:
+            self.__current_bid = bid
+            return None
+
+    def can_accept_bid(self):
+        return not self.__current_bid
 
     def handle(self):
-        if self.status == 0:
+        if not self.__current_bid:
             return
         temp = random.randint(0, 100)
-        if self.next_element.status == 0 or type(self.next_element) is Counter:
-            if temp >= self.__probability:
-                self.status = 0
-                self.next_element.status += 1
+        if temp >= self.__probability:
+            self.__current_bid = self.next_element.process(self.__current_bid)
 
 
 class HandlerWithReject(BaseElement):
@@ -68,25 +81,37 @@ class HandlerWithReject(BaseElement):
     def __init__(self, probability, next_element):
         super().__init__(next_element)
         self.__probability = probability
-        self.__number_rejected = 0
+        self.__rejected_counter = Counter()
+        self.__current_bid = None
+
+    def status(self):
+        return 1 if self.__current_bid else 0
 
     @property
-    def number_rejected(self):
-        return self.__number_rejected
+    def rejected_counter(self):
+        return self.__rejected_counter
+
+    def process(self, bid):
+        if self.__current_bid:
+            return bid
+        else:
+            self.__current_bid = bid
+            return None
+
+    def can_accept_bid(self):
+        return not self.__current_bid
 
     def handle(self):
-        if self.status == 0:
+        if not self.__current_bid:
             return
         temp = random.randint(0, 100)
-        if self.next_element.status == 0 or type(self.next_element) is Counter:
+        if self.next_element.can_accept_bid():
             if temp >= self.__probability:
-                self.status = 0
-                self.next_element.status += 1
-                return
-        if temp < self.__probability and self.next_element.status > 0:
-            self.status = 0
-            self.__number_rejected += 1
-            return
+                self.__current_bid = self.next_element.process(self.__current_bid)
+        else:
+            if temp < self.__probability:
+                self.__rejected_counter.process(self.__current_bid)
+                self.__current_bid = None
 
 
 class Queue(BaseElement):
@@ -94,35 +119,69 @@ class Queue(BaseElement):
     def __init__(self, capacity, next_element):
         super().__init__(next_element)
         self.__capacity = capacity
-        self.__busy_places = 0
-        self.__number_elements = 0
+        self.__queue = deque([])
+        self.__number_bids = 0
 
-    def is_empty(self):
-        return self._status == 0
-
-    def put_in_queue(self):
-        if self.status == 0 and self.next_element.status == 0:
-            self.next_element.status += 1
-        elif not self.is_full():
-            self.status += 1
-
-    def is_full(self):
-        return self._status == self.__capacity
+    def status(self):
+        return len(self.__queue)
 
     @property
-    def number_elements(self):
-        return self.__number_elements
+    def number_bids(self):
+        return self.__number_bids
+
+    def process(self, bid):
+        if len(self.__queue) == self.__capacity:
+            return bid
+        elif self.__queue:
+            self.__queue.append(bid)
+            return None
+        else:
+            temp_bid = self.next_element.process(bid)
+            if temp_bid:
+                self.__queue.append(bid)
+                bid.time_in_queue += 1
+            return None
+
+    def can_accept_bid(self):
+        return len(self.__queue) < self.__capacity
 
     def handle(self):
-        if self.status == 0:
-            return
-        self.__number_elements += self.status
-        if self.next_element.status == 0:
-            self.next_element.status += 1
-            self.status -= 1
+        if self.__queue:
+            self.__number_bids += len(self.__queue)
+            bid = self.next_element.process(self.__queue[0])
+            if not bid:
+                self.__queue.popleft()
+        for bid in self.__queue:
+            bid.time_in_queue += 1
 
 
 class Counter(BaseElement):
 
     def __init__(self):
         super().__init__(None)
+        self.__bids = []
+
+    @property
+    def bids(self):
+        return self.__bids
+
+    def can_accept_bid(self):
+        return True
+
+    def process(self, bid):
+        self.__bids.append(bid)
+        return None
+
+
+class Bid:
+
+    def __init__(self):
+        self.__time_in_queue = 0
+
+    @property
+    def time_in_queue(self):
+        return self.__time_in_queue
+
+    @time_in_queue.setter
+    def time_in_queue(self, time_in_queue):
+        self.__time_in_queue = time_in_queue
--- SAaMMod/4lab/main.py
+++ SAaMMod/4lab/main.py
@@ -1,5 +1,6 @@
 from elements import Queue, Handler, HandlerWithReject, Counter, Generator
 
+
 STATES_PROBABILITY = {'000': 0, '010': 0, '001': 0, '110': 0, '011': 0, '111': 0, '211': 0, '210': 0}
 
 
@@ -8,11 +9,11 @@ def main():
     # pi1 = int(input('Enter pi1: '))
     # pi2 = int(input('Enter pi2: '))
     # queue_capacity = int(input('Enter queue capacity: '))
-    q = 50
-    pi1 = 40
-    pi2 = 40
+    q = 50.0
+    pi1 = 40.0
+    pi2 = 40.0
     queue_capacity = 2
-    number_steps = 10000
+    number_steps = 100000
 
     counter = Counter()
     handler = Handler(pi2, counter)
@@ -29,17 +30,20 @@ def main():
     for i in range(number_steps):
         for element in elements:
             element.handle()
-        state = str(queue.status) + str(handler_with_reject.status) + str(handler.status)
+        state = str(queue.status()) + str(handler_with_reject.status()) + str(handler.status())
         STATES_PROBABILITY[state] += 1
 
-    a = counter.status / number_steps
-    l = queue.number_elements / number_steps
-    w = l / a
-    q = counter.status / generator.generated_number
+    a = len(counter.bids) / number_steps
+    l = queue.number_bids / number_steps
+    q = len(counter.bids) / generator.generated_number
 
     print()
     for key, value in STATES_PROBABILITY.items():
         print('%s : %s' % (key, str(value / number_steps)))
+        STATES_PROBABILITY[key] = float(value / number_steps)
+
+    bids_time_in_queue = [bid.time_in_queue for bid in handler_with_reject.rejected_counter.bids + counter.bids]
+    w = sum(bids_time_in_queue) / generator.generated_number
 
     print('\nA = ' + str(a))
     print('L = ' + str(l))
